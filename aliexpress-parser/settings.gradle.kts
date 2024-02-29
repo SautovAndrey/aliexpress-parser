29d3b4efe8f94c8441ba4453bb96b6e185f4aaf7
Sautov <seeee8212@gmail.com>
2024-02-29 13:42:09 +0300

    Initial commit

---
+++ aliexpress-parser/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "aliexpress-parser"
---
+++ aliexpress-parser/src/main/java/com/joodjoy/aliexpressparser/AliExpressParserApplication.java
@@ -0,0 +1,13 @@
+package com.joodjoy.aliexpressparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliExpressParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliExpressParserApplication.class, args);
+	}
+
+}
